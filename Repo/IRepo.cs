37dcbaf5ec556dbe846c97da689fe9b61eb4ef6e
Andrei Zvonimir Crnkovic <crnkovic.a.z@gmail.com>
2015-03-10 20:01:45 +0100

    Repo init :beer:

---
+++ Repo/IRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Devices;
+
+namespace Repo
+{
+    public interface IRepo
+    {
+        void Save(Device device);
+        List<Device> Find();
+        List<Device> Find(string deviceName);
+    }
+}
