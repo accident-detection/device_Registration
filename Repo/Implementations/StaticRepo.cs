f1b41de820ad512aefbc557fbe66e2c38118eff9
Andrei Zvonimir Crnkovic <crnkovic.a.z@gmail.com>
2015-03-11 20:01:45 +0100

    Started implementation of a List repo

---
+++ Repo/Implementations/StaticRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo.Implementations
+{
+    class StaticRepo : IRepo
+    {
+        public void Save(Device device)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Device> Find()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Device> Find(string deviceName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
